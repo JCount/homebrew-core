c8980683b1f7b8de8e749ac274004255ddfa7b0d
JCount <JCount42@gmail.com>
2017-08-11 15:56:30 -0400

    imagemagick: add option to build with libraw for RAW support
    since HDRI support is built by default, the current `with-hdri`
    option is a no-op, so replace it with an option to explicitly
    disable HDRI. also, refactor formula to be more readable.

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -16,41 +16,43 @@ class Imagemagick < Formula
   end
 
   option "with-fftw", "Compile with FFTW support"
-  option "with-hdri", "Compile with HDRI support"
+  option "with-libraw", "Compile with RAW support"
   option "with-opencl", "Compile with OpenCL support"
   option "with-openmp", "Compile with OpenMP support"
   option "with-perl", "Compile with PerlMagick"
-  option "without-magick-plus-plus", "disable build/install of Magick++"
+  option "without-hdri", "Disable HDRI support"
+  option "without-magick-plus-plus", "Disable build/install of Magick++"
   option "without-modules", "Disable support for dynamically loadable modules"
   option "without-threads", "Disable threads support"
   option "with-zero-configuration", "Disables depending on XML configuration files"
 
-  deprecated_option "enable-hdri" => "with-hdri"
   deprecated_option "with-jp2" => "with-openjpeg"
 
   depends_on "pkg-config" => :build
   depends_on "libtool" => :run
   depends_on "xz"
 
+  depends_on "freetype" => :recommended
   depends_on "jpeg" => :recommended
   depends_on "libpng" => :recommended
   depends_on "libtiff" => :recommended
-  depends_on "freetype" => :recommended
 
-  depends_on :x11 => :optional
+  depends_on "fftw" => :optional
   depends_on "fontconfig" => :optional
+  depends_on "ghostscript" => :optional
+  depends_on "liblqr" => :optional
+  depends_on "libraw" => :optional
+  depends_on "librsvg" => :optional
+  depends_on "libwmf" => :optional
   depends_on "little-cms" => :optional
   depends_on "little-cms2" => :optional
-  depends_on "libwmf" => :optional
-  depends_on "librsvg" => :optional
-  depends_on "liblqr" => :optional
   depends_on "openexr" => :optional
-  depends_on "ghostscript" => :optional
-  depends_on "webp" => :optional
   depends_on "openjpeg" => :optional
-  depends_on "fftw" => :optional
   depends_on "pango" => :optional
+  depends_on "webp" => :optional
+
   depends_on :perl => ["5.5", :optional]
+  depends_on :x11 => :optional
 
   needs :openmp if build.with? "openmp"
 
@@ -66,6 +68,12 @@ class Imagemagick < Formula
       --enable-static
     ]
 
+    if build.without? "hdri"
+      args << "--disable-hdri"
+    else
+      args << "--enable-hdri"
+    end
+
     if build.without? "modules"
       args << "--without-modules"
     else
@@ -76,6 +84,12 @@ class Imagemagick < Formula
       args << "--enable-opencl"
     else
       args << "--disable-opencl"
+    end
+
+    if build.with? "openjpeg"
+      args << "--with-openjp2"
+    else
+      args << "--without-openjp2"
     end
 
     if build.with? "openmp"
@@ -90,26 +104,20 @@ class Imagemagick < Formula
       args << "--without-webp"
     end
 
-    if build.with? "openjpeg"
-      args << "--with-openjp2"
-    else
-      args << "--without-openjp2"
-    end
-
-    args << "--without-gslib" if build.without? "ghostscript"
-    args << "--with-perl" << "--with-perl-options='PREFIX=#{prefix}'" if build.with? "perl"
-    args << "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts" if build.without? "ghostscript"
-    args << "--without-magick-plus-plus" if build.without? "magick-plus-plus"
-    args << "--enable-hdri=yes" if build.with? "hdri"
     args << "--without-fftw" if build.without? "fftw"
-    args << "--without-pango" if build.without? "pango"
-    args << "--without-threads" if build.without? "threads"
-    args << "--with-rsvg" if build.with? "librsvg"
-    args << "--without-x" if build.without? "x11"
     args << "--with-fontconfig=yes" if build.with? "fontconfig"
     args << "--with-freetype=yes" if build.with? "freetype"
-    args << "--enable-zero-configuration" if build.with? "zero-configuration"
+    args << "--without-gslib" if build.without? "ghostscript"
+    args << "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts" if build.without? "ghostscript"
+    args << "--without-raw" if build.without? "libraw"
+    args << "--with-rsvg" if build.with? "librsvg"
     args << "--without-wmf" if build.without? "libwmf"
+    args << "--without-magick-plus-plus" if build.without? "magick-plus-plus"
+    args << "--without-pango" if build.without? "pango"
+    args << "--with-perl" << "--with-perl-options='PREFIX=#{prefix}'" if build.with? "perl"
+    args << "--without-threads" if build.without? "threads"
+    args << "--without-x" if build.without? "x11"
+    args << "--enable-zero-configuration" if build.with? "zero-configuration"
 
     # versioned stuff in main tree is pointless for us
     inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_VERSION}", "${PACKAGE_NAME}"
